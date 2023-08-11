6a51c107d1feb7657b2acd1af7e5dd62da77cd62
Cendrine Briard <cendrine_briard@protonmail.com>
2023-08-11 13:37:27 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Locations from './components/Locations/Locations.js';
+import About from './components/About/About.js';
+import Error from './components/Error/Error.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/logements' element={<Locations />} />
+  <Route path='/a-propos' element={<About />} />
+  <Route path='/*' element={<Error />} />
+</Routes>
   );
 }
 
---
+++ src/components/Locations/Locations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ function Locations() {
+    return (
+        <div>
+            <h1> Location</h1>
+        </div>
+    )
+};
+
+ export default Locations
